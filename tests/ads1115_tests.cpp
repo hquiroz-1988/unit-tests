d3df78c183a7c794cf235ce530fcfaa076c4fd60
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2025-06-30 23:14:52 -0700

    creating test file

---
+++ tests/ads1115_tests.cpp
@@ -0,0 +1,27 @@
+
+#include "CppUTest/TestHarness.h"
+#include "CppUTestExt/MockSupport.h"
+
+#include "ads1115.hpp"
+
+extern "C"
+{
+    
+}
+
+TEST_GROUP(ADS1115_tests)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+
+    }
+};
+
+TEST(ADS1115_tests, taskRun)
+{
+    
+}
