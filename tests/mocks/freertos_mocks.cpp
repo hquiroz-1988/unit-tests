4a730c24f1911e82440e317cf2249556a54b52d7
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2025-07-01 23:50:52 -0700

    creating freertos mock functions

---
+++ tests/mocks/freertos_mocks.cpp
@@ -0,0 +1,35 @@
+#include "FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+#include "CppUTestExt/MockSupport.h"
+
+extern "C"
+{
+
+}
+
+#include <stdint.h>
+#include <stddef.h>
+
+
+
+void* xTaskGetCurrentTaskHandle() {
+    return mock().actualCall("xTaskGetCurrentTaskHandle").returnPointerValueOrDefault(nullptr);
+}
+
+uint32_t ulTaskNotifyTake( BaseType_t xClearCountOnExit, TickType_t xTicksToWait ) {
+    return mock().actualCall("ulTaskNotifyTake")
+        .withParameter("xClearCountOnExit", xClearCountOnExit)
+        .withParameter("xTicksToWait", xTicksToWait)
+        .returnUnsignedLongIntValueOrDefault(0);
+}
+
+BaseType_t xQueueGenericSend( QueueHandle_t xQueue, const void * const pvItemToQueue, TickType_t xTicksToWait, const BaseType_t xCopyPosition )
+{
+    return mock().actualCall("xQueueGenericSend")
+        .withPointerParameter("xQueue", xQueue)
+        .withConstPointerParameter("pvItemToQueue", pvItemToQueue)
+        .withParameter("xTicksToWait", xTicksToWait)
+        .withParameter("xCopyPosition", xCopyPosition)
+        .returnIntValueOrDefault(0);
+}   
