19fc08c2a7355d00d9225545491328d1e40bd139
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2025-07-19 23:42:10 -0700

    tested all test cases for task.hpp

--- tests/src/power_monitor_tests.cpp
+++ tests/src/power_monitor_tests.cpp
@@ -47,29 +47,44 @@ TEST(PowerMonitor_tests, Constructor)
 
 TEST(PowerMonitor_tests, initTask)
 {
+    /*  Test with non null task handle  */
+    mock().expectOneCall("CHECK_POINTER_VALID")
+        /*  we dont care what is passed into check pointer valid */
+        .ignoreOtherParameters()
+        /* but well return true for testing purposes    */
+        .andReturnValue(true);
+    retVal_t ret = powerMonitor->initTask();
+    CHECK_EQUAL(ERR_FAIL, ret);
     
 
     /*  Test with xport returning in ISR    */
+    mock().expectOneCall("CHECK_POINTER_VALID")
+        /*  we dont care what is passed into check pointer valid */
+        .ignoreOtherParameters()
+        /* but well return false to proceed with other tests */
+        .andReturnValue(false);
     mock().expectOneCall("xPortInIsrContext").andReturnValue(1);
-    retVal_t ret = powerMonitor->initTask();
+    ret = powerMonitor->initTask();
     /* this should return fail but not call any other functions  */
     CHECK_EQUAL(ERR_IN_ISR, ret);
-
-
-    /* create real memory block, wont be used but needs to be a real location in memory */
-    const StaticTask_t memBlock = {0};
-
+    
+    
+    /*  expect call to check pointer valid */
+    mock().expectOneCall("CHECK_POINTER_VALID")
+        /*  we dont care what is passed into check pointer valid */
+        .ignoreOtherParameters()
+        /* but well return false to proceed with other tests */
+        .andReturnValue(false);
     /*  Test with xport returning not in ISR    */
     mock().expectOneCall("xPortInIsrContext").andReturnValue(0);
     
-    /* expect xTaskCreate to not run smoothly   */
+    /* return a fail from xtaskcreate   */
     mock().expectOneCall("xTaskCreate")
     .withBoolParameter("pxTaskCode", true) /* this shoudl return true  since function ptr should not be nullptr */
     .withStringParameter("pcName", "PowerMonitor")
     .withBoolParameter("usStackDepth", true) /* this should be true if stack size greater than min */
     .withPointerParameter("pvParameters", powerMonitor)
     .withParameter("uxPriority", ESP_NORMAL_PRIORITY)
-    .withOutputParameterReturning("pxCreatedTask", &memBlock, sizeof(StaticTask_t *))
     .ignoreOtherParameters()
     .andReturnValue(pdFAIL);
     
@@ -77,17 +92,23 @@ TEST(PowerMonitor_tests, initTask)
     
     CHECK_EQUAL(ERR_OS_FAIL, ret);
 
+
+    /*  expect call to check pointer valid */
+    mock().expectOneCall("CHECK_POINTER_VALID")
+        /*  we dont care what is passed into check pointer valid */
+        .ignoreOtherParameters()
+        /* but well return false to proceed with other tests */
+        .andReturnValue(false);
     /*  Test with xport returning not in ISR    */
     mock().expectOneCall("xPortInIsrContext").andReturnValue(0);
     
-    /* expect xTaskCreate to not run smoothly   */
+    /* return pass from xtaskcreate */
     mock().expectOneCall("xTaskCreate")
     .withBoolParameter("pxTaskCode", true) /* this shoudl return true  since function ptr should not be nullptr */
     .withStringParameter("pcName", "PowerMonitor")
     .withBoolParameter("usStackDepth", true) /* this should be true if stack size greater than min */
     .withPointerParameter("pvParameters", powerMonitor)
     .withParameter("uxPriority", ESP_NORMAL_PRIORITY)
-    .withOutputParameterReturning("pxCreatedTask", &memBlock, sizeof(StaticTask_t *))
     .ignoreOtherParameters()
     .andReturnValue(pdPASS);
     
@@ -96,11 +117,87 @@ TEST(PowerMonitor_tests, initTask)
     CHECK_EQUAL(ERR_NONE, ret);
 }
 
-// TEST(PowerMonitor_tests, taskRun)
-// {
-//     // mock("BusVoltage")
-//     //     .expectOneCall("getVoltage")
-//     //     .andReturnValue(230.0f);
+TEST(PowerMonitor_tests, runInCurrent)
+{
+    /* expect taskRun to be called */
+    mock().expectNCalls(2, "FOREVER")
+        .andReturnValue(true)
+        .andReturnValue(false);
+    
+    powerMonitor->runInCurrent();
+}
 
-//     // taskRun();
-// }
+TEST(PowerMonitor_tests, SuspendAndResume)
+{
+    /*  Make a call to task suspend with an unitialized taskhandle  */
+    mock().expectOneCall("CHECK_POINTER_VALID")
+        /* this might or might not be false, not important for this test */
+        .withBoolParameter("ptr", false)
+        /* we want this to return false to simulate error */
+        .andReturnValue(false);
+    retVal_t ret = powerMonitor->suspend();
+    /* if task handle uninitialized this should return null pointer  */
+    CHECK_EQUAL(ERR_NULL_POINTER, ret);
+
+
+    /*  Make a call to task resume with an unitialized taskhandle  */
+    mock().expectOneCall("CHECK_POINTER_VALID")
+        /* this might or might not be false, not important for this test */
+        .withBoolParameter("ptr", false)
+        /* we want this to return false to simulate error */
+        .andReturnValue(false);
+    /*  Test with null task handle    */
+    ret = powerMonitor->resume();
+    /* this should return null pointer  */
+    CHECK_EQUAL(ERR_NULL_POINTER, ret);
+
+    /*  Test with xport returning in ISR    */
+    mock().expectOneCall("CHECK_POINTER_VALID")
+        /* this might or might not be false, not important for this test */
+        .withBoolParameter("ptr", false)
+        /* we want this to return true so we can continue to other tests*/
+        .andReturnValue(true);
+    mock().expectOneCall("xPortInIsrContext").andReturnValue(1);
+    ret = powerMonitor->suspend();
+    /* this should return fail but not call any other functions  */
+    CHECK_EQUAL(ERR_IN_ISR, ret);
+
+    /*  Test with xport returning not in ISR    */
+    mock().expectOneCall("CHECK_POINTER_VALID")
+        /* this might or might not be false, not important for this test */
+        .withBoolParameter("ptr", false)
+        /* we want this to return true so we can continue to other tests*/
+        .andReturnValue(true);
+    mock().expectOneCall("xPortInIsrContext").andReturnValue(0);
+    /* expect vTaskSuspend to be called */
+    mock().expectOneCall("vTaskSuspend")
+        /* we dont care about the value of xTaskToSuspend since we are mocking it */    
+        .ignoreOtherParameters();
+    ret = powerMonitor->suspend();
+    CHECK_EQUAL(ERR_NONE, ret);
+
+    /*  Test with xport returning in ISR    */
+    mock().expectOneCall("CHECK_POINTER_VALID")
+        /* this might or might not be false, not important for this test */
+        .withBoolParameter("ptr", false)
+        /* we want this to return true so we can continue to other tests*/
+        .andReturnValue(true);
+    mock().expectOneCall("xPortInIsrContext").andReturnValue(1);
+    ret = powerMonitor->resume();
+    /* this should return in isr error */
+    CHECK_EQUAL(ERR_IN_ISR, ret);
+
+    /*  Test with xport returning not in ISR    */
+    mock().expectOneCall("CHECK_POINTER_VALID")
+        /* this might or might not be false, not important for this test */
+        .withBoolParameter("ptr", false)
+        /* we want this to return true so we can continue to other tests*/
+        .andReturnValue(true);
+    mock().expectOneCall("xPortInIsrContext").andReturnValue(0);
+    /* expect vTaskResume to be called */
+    mock().expectOneCall("vTaskResume")
+        /* ignore value into vtask resume since we are mocking handle above */
+        .ignoreOtherParameters();
+    ret = powerMonitor->resume();
+    CHECK_EQUAL(ERR_NONE, ret);
+}
